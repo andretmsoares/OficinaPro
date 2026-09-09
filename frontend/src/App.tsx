7e55f4be8184195038c70b31214da1e21137cd40
andretmsoares <andre.tharssys.marques.soares@ccc.ufcg.edu.br>
2026-09-09 15:29:13 -0300

    feat: init page mecanico

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { Clientes } from "./pages/Clientes";
 import { Login } from "./pages/Login";
 import { Veiculos } from "./pages/Veiculos";
 import { OrdemDeServico } from "./pages/OrdensServico";
+import { Mecanicos } from "./pages/Mecanicos";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
@@ -29,7 +30,7 @@ export default function App() {
           <Route path="/clientes" element={<Clientes />} />
           <Route path="/veiculos" element={<Veiculos />} />
           <Route path="/ordens-servico" element={<OrdemDeServico />} />
-          <Route path="/mecanicos" element={<div>Mecânicos</div>} />
+          <Route path="/mecanicos" element={<Mecanicos />} />
           <Route path="/pecas" element={<div>Peças</div>} />
           <Route path="/distribuidoras" element={<div>Distribuidoras</div>} />
           <Route path="/pagamentos" element={<div>Pagamentos</div>} />
